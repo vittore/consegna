f6de814df440665cfbc97bb91037dfd50afe4dab
Vittore Zen <vittore@zen.pn.it>
2012-05-21 16:53:24 +0200

    readme.md

--- src/Consegna/ElaboratiBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Consegna/ElaboratiBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,15 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+         
+        $crawler = $client->request('GET', '/');
+        
+        
+         $crawler = $crawler->filter('img');
+         foreach ($crawler as $elementoDom) {
+    print $elementoDom->nodeValue;
+}
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+     //   $this->assertEquals('consegna', $link->getUri(), "Consegna link found on page");
     }
 }
